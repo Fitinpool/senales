b40b524fe0082d6ec29fdcb1743c1923adc29323
Fitinpool <julio.barravalenzuela@gmail.com>
2022-04-20 15:50:09 -0400

    PARTE 3 Y 4 || listo para hacer el for , se hace el segundo y listo el 5 da lo mismo

--- 1.py
+++ 1.py
@@ -1,11 +1,13 @@
 #LIBRERIAS PARA FUNCIONES
+from cmath import exp
+from random import triangular
 import numpy as np
 from scipy import signal
 import matplotlib. pyplot as plt
 
+"""PARTE 1
 #VARIABLES SINOIDAL
-signalTime = np.arange(5, 20, 0.1); 
-  
+signalTime = np.arange(-5, 5, 0.1); 
 signalAmplitude = np.sin(signalTime)
 
 #VARIABLE CUADRADA, TRIANGULAR Y DIENTE DE SIERRA
@@ -14,11 +16,9 @@ b = 2
 n = 2000
 fs = 5
 
-#t = np.linspace(a, b, n, endpoint=True)
-T = 0.01
-t = np.arange(-2,2 +T, T)
+t = np.linspace(a, b, n, endpoint=True)
 
-fig1 = plt.figure("SEÑALES")
+fig1 = plt.figure("PARTE 1")
 fig1.subplots_adjust(hspace=0.5, wspace=0.5)
 
 #ORDEN DE LAS SEÑALES
@@ -33,7 +33,7 @@ ax = fig1.add_subplot(2,2,2)
 ax.set_ylabel('Amplitud')
 ax.set_xlabel('Tiempo')
 ax.set_title('CUADRADA')
-ax.plot(t, signal.square(np.pi*t))
+ax.plot(t, signal.square(2 * np.pi * fs * t))
 
 ax = fig1.add_subplot(2,2,3)
 ax.set_ylabel('Amplitud')
@@ -48,30 +48,32 @@ ax.set_title('DIENTE DE SIERRA')
 ax.plot(t , signal.sawtooth(2 * np.pi * fs * t))
 
 plt.show()
-"""Parte 2
+"""
+"""PARTE 2
+#PARTE2
 #EXPONENCIAL DECRECIENTE
 #VARIABLES
 u = lambda t: np.piecewise(t,t>=0,[1,0])
 
-a = -5
-b = 5
+a = -2
+b = 2
 dt = 0.1
 t = np.arange(a, b, dt)
+
+fig2 = plt.figure("PARTE2")
+fig2.subplots_adjust(hspace=0.5, wspace=0.5)
+
 
 # PROCEDIMIENTO
 
 ExpDecreciente = np.exp(-t) * (u(t) - u(t - 1))
 
 # SALIDA - GRAFICA
-plt.figure(2)
-plt.plot(t, ExpDecreciente)
-
-plt.title('EXPONENCIAL DECRECIENTE')
-plt.xlabel('t')
-plt.ylabel('impulso')
-plt.margins(dt)
-plt.grid()
-plt.show()
+ax = fig2.add_subplot(2,2,1)
+ax.set_ylabel('impulso')
+ax.set_xlabel('t')
+ax.set_title('EXPONENCIAL DECRACIENTE')
+ax.plot(t,ExpDecreciente)
 
 #EXPONENCIAL CRECIENTE
 # PROCEDIMIENTO
@@ -79,15 +81,11 @@ plt.show()
 ExpCreciente = np.exp(t) * (u(t) - u(t - 1))
 
 # SALIDA - GRAFICA
-plt.figure(2)
-plt.plot(t,ExpCreciente)
-
-plt.title('EXPONENCIAL CRECIENTE')
-plt.xlabel('t')
-plt.ylabel('impulso')
-plt.margins(dt)
-plt.grid()
-plt.show()
+ax = fig2.add_subplot(2,2,2)
+ax.set_ylabel('impulso')
+ax.set_xlabel('t')
+ax.set_title('EXPONENCIAL CRECIENTE')
+ax.plot(t,ExpCreciente)
 
 #IMPULSO  
 # PROCEDIMIENTO
@@ -95,15 +93,11 @@ plt.show()
 impulso = u(t) - u(t - 1)
 
 # SALIDA - GRAFICA
-plt.figure(2)
-plt.plot(t,impulso)
-
-plt.title('IMPULSO')
-plt.xlabel('t')
-plt.ylabel('impulso')
-plt.margins(dt)
-plt.grid()
-plt.show()
+ax = fig2.add_subplot(2,2,3)
+ax.set_ylabel('impulso')
+ax.set_xlabel('t')
+ax.set_title('IMPULSO')
+ax.plot(t,impulso)
 
 #ESCALON
 # PROCEDIMIENTO
@@ -111,69 +105,57 @@ plt.show()
 escalon = u(t)
 
 # SALIDA - GRAFICA
-plt.figure(2)
-plt.plot(t,escalon)
-
-plt.title('ESCALON')
-plt.xlabel('t')
-plt.ylabel('impulso')
-plt.margins(dt)
-plt.grid()
+ax = fig2.add_subplot(2,2,4)
+ax.set_ylabel('impulso')
+ax.set_xlabel('t')
+ax.set_title('ESCALON')
+ax.plot(t,escalon)
 plt.show()
-
 #Sinc(x)
 # PROCEDIMIENTO
 
 sinc = np.sin(t)/t
 
-# SALIDA - GRAFICA
-plt.figure(2)
-plt.plot(t,sinc)
+fig3 = plt.figure("PARTE 2")
+fig3.subplots_adjust(hspace=0.5, wspace=0.5)
 
-plt.title('SINC')
-plt.xlabel('t')
-plt.ylabel('impulso')
-plt.margins(dt)
-plt.grid()
+# SALIDA - GRAFICA
+ax = fig3.add_subplot(2,2,1)
+ax.set_ylabel('impulso')
+ax.set_xlabel('t')
+ax.set_title('SINC')
+ax.plot(t,sinc)
+
 plt.show()
 """
-
-"""
+"""PARTE 3
+#PARTE 3
 # INGRESO
 # tiempo [a,b] simétrico alrededor de 0
-b = 5 ; a = -b; dt =0.01
+b = 2 ; a = -b; dt =0.01; fs = 5
 
 u = lambda t: np.piecewise(t,t>=0,[1,0])
 
-x = lambda t: 10*np.exp(-3*t)*u(t)
-h = lambda t: (2*np.exp(-2*t)-np.exp(-t))*u(t)
-
-# PROCEDIMIENTO
-a1 = 0
-b2 = 1
-n1 = 10
-fs1 = 5
-
-t = np.linspace(a1, b2,1000, endpoint=True)
+x = lambda t: signal.square(2 * np.pi * fs * t)
+h = lambda t: np.sin(t)/t
 
 
 # PROCEDIMIENTO
-ti = np.arange(a, b,dt); 
-xi = signal.square(2 * np.pi * fs1 * ti)
-hi = np.sin(ti)/ti
+ti = np.arange(a,b+dt,dt)
+xi = x(ti)
+hi = h(ti)
 
 # Integral de Convolucion x[t]*h[t]
 # corrección de magnitud por dt para en integral
 yi = np.convolve(xi,hi,'same')*dt
 
 # SALIDA - GRAFICA
-plt.figure(1)
-plt.suptitle('Integral de Convolución x(t)*h(t)')
+plt.figure('PARTE 3')
+plt.suptitle('Convolucion')
 
 plt.subplot(211)
 plt.plot(ti,xi,'b', label='x(t)')
 plt.plot(ti,hi,'r', label='h(t)')
-plt.margins(dt)
 plt.legend()
 plt.grid()
 
@@ -184,4 +166,64 @@ plt.legend()
 plt.grid()
 
 plt.show()
+"""
+"""PARTE 4
+#PARTE 4
+
+#EXPONENCIAL DECRECIENTE
+#VARIABLES
+u = lambda t: np.piecewise(t,t>=0,[1,0])
+
+b = 2 ; a = -b; dt =0.01; fs = 5
+
+# PROCEDIMIENTO
+#aperiodica
+ExpDecreciente = lambda t:(np.exp(-t) * (u(t) - u(t-1)))
+ExpCreciente = lambda t: np.exp(t) * (u(t) - u(t - 1))
+impulso = lambda t : u(t) - u(t - 1)
+escalon = lambda t: u(t)
+sinc = lambda t: np.sin(t)/t
+
+#periodica
+sinoidal = lambda t: np.sin(t)
+cuadrada = lambda t: signal.square(2 * np.pi * fs * t)
+triangular = lambda t: signal.sawtooth(2 * np.pi * fs * t, 0.5)
+dienteSierra = lambda t: signal.sawtooth(2 * np.pi * fs * t)
+
+
+aperiodicas = {'DECRECIENTE' : ExpDecreciente, 'CRECIENTE' : ExpCreciente, 'IMPULSO' : impulso, 'ESCALON' : escalon, 'SINC', sinc}
+periodicas = {'SINOIDAL' : sinoidal, 'CUADRADA' : cuadrada, 'TRIANGULAR' : triangular, 'DIENTESIERRA' : dienteSierra}
+
+i = 1
+for x,y in aperiodicas.items():
+    fig4 = plt.figure("PARTE2")
+    fig4.subplots_adjust(hspace=0.5, wspace=0.5)
+
+    g = y(t)
+    ax = fig4.add_subplot(2,2,2)
+    ax.set_ylabel('impulso')
+    ax.set_xlabel('t')
+    ax.set_title(x + ' sin modificacion')
+    ax.plot(t,g)
+
+    h = 2*y(t-0.5)
+
+    ax = fig4.add_subplot(2,1,2)
+    ax.set_ylabel('impulso')
+    ax.set_xlabel('t')
+    ax.set_title(x + ' amplificada por 2 y movida 0.5')
+    ax.plot(t,h)
+
+    plt.show()
+"""
+
+"""PARTE 5
+#PARTE 5
+
+#SEÑALES PERIODICAS
+
+sinoidal = lambda t: np.sin(t)
+cuadrada = lambda t: signal.square(2 * np.pi * fs * t)
+triangular = lambda t: signal.sawtooth(2 * np.pi * fs * t, 0.5)
+dienteSierra = lambda t: signal.sawtooth(2 * np.pi * fs * t)
 """
